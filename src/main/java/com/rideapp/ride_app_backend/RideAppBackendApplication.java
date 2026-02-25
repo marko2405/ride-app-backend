1c0b68fd421a120cd4b4f1860639b7b9be98ba1b
Marko Stojanović <stojanovicmarko688@gmail.com>
2026-02-25 19:50:13 +0100

    Initial commit with Spring Boot config placeholders

---
+++ src/main/java/com/rideapp/ride_app_backend/RideAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rideapp.ride_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rideapp/ride_app_backend/RideAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rideapp.ride_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
